97de8c949766ffe65ca9daf0a1a8f668c9485aef
Decjam <s210542@aid.edu.au>
2021-03-09 16:20:20 +1100

    Added MathLibrary with most (not all) of the Vector stuff done

---
+++ MathLibrary/Math.cs
@@ -0,0 +1,220 @@
+﻿using System;
+
+namespace MathLibrary
+{
+	public struct Vector2
+	{
+		public float x;
+		public float y;
+
+		public Vector2(float x = 0f, float y = 0f)
+		{
+			this.x = x;
+			this.y = y;
+		}
+		//Operator overloading
+
+		// V = V + V (point translated by a vector)
+		public static Vector2 operator + (Vector2 lhs, Vector2 rhs)
+		{
+			Vector2 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+
+			return result;
+		}
+
+		// V = V –V (point translated by a vector)
+		public static Vector2 operator -(Vector2 lhs, Vector2 rhs)
+		{
+			Vector2 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+
+			return result;
+		}
+		// V = V x f(vector scale)
+		public static Vector2 operator*(Vector2 lhs, float rhs)
+		{
+			Vector2 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			return result;
+		}
+		// V = f x V (vector scale)
+		public static Vector2 operator *(float lhs, Vector2 rhs)
+		{
+			Vector2 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			return result;
+		}
+		// f = V.Magnitude()
+		public float Magnitude()
+		{
+			return (float)Math.Sqrt((x * x) + (y * y));
+		}
+		// V.Normalise()
+		public float Normalise()
+		{
+			float magnitude = Magnitude();
+			if(magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				return magnitude;
+			}
+			return magnitude;
+		}
+	}
+	public struct Vector3
+	{
+		public float x;
+		public float y;
+		public float z;
+
+		public Vector3(float x = 0f, float y = 0f, float z = 0f)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+		//Operator overloading
+
+		// V = V + V (point translated by a vector)
+		public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+
+			return result;
+		}
+
+		// V = V –V (point translated by a vector)
+		public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+			return result;
+		}
+		// V = V x f(vector scale)
+		public static Vector3 operator *(Vector3 lhs, float rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+			return result;
+		}
+		// V = f x V (vector scale)
+		public static Vector3 operator *(float lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+			return result;
+		}
+		// f = V.Magnitude()
+		public float Magnitude()
+		{
+			return (float)Math.Sqrt((x * x) + (y * y) + (z * z));
+		}
+		// V.Normalise()
+		public float Normalise()
+		{
+			float magnitude = Magnitude();
+			if (magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				z /= magnitude;
+				return magnitude;
+			}
+			return magnitude;
+			
+		}
+	}
+	public struct Vector4
+	{
+		public float x;
+		public float y;
+		public float z;
+		public float w;
+
+		public Vector4(float x = 0f, float y = 0f, float z = 0f, float w = 0f)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+		//Operator overloading
+
+		// V = V + V (point translated by a vector)
+		public static Vector4 operator +(Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+			result.w = lhs.w + rhs.w;
+			return result;
+		}
+
+		// V = V –V (point translated by a vector)
+		public static Vector4 operator -(Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+			result.w = lhs.w - rhs.w;
+			return result;
+		}
+		// V = V x f(vector scale)
+		public static Vector4 operator *(Vector4 lhs, float rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+			result.w = lhs.w * rhs;
+			return result;
+		}
+		// V = f x V (vector scale)
+		public static Vector4 operator *(float lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+			result.w = lhs * rhs.w;
+			return result;
+		}
+		// f = V.Magnitude()
+		public float Magnitude()
+		{
+			return (float)Math.Sqrt((x * x) + (y * y) + (z * z) + (w * w));
+		}
+		// V.Normalise()
+		public float Normalise()
+		{
+			float magnitude = Magnitude();
+			if (magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				z /= magnitude;
+				w /= magnitude;
+				return magnitude;
+			}
+			return magnitude;
+
+		}
+	}
+}
