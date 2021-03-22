bb4e993c6976ab780e4ea7e195a420132e5b1a1b
Decjam <s210542@aid.edu.au>
2021-03-22 11:37:35 +1100

    Completed
    Split single .cs into separate .cs files and pulled the code from the Unit test to make sure it all works

---
+++ MathLibrary/Colour.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public struct Colour
+    {
+		public uint colour;
+
+		public Colour(byte red, byte green, byte blue, byte alpha)
+		{
+			colour = (uint)((red << 24) + (green << 16) + (blue << 8) + alpha);
+		}
+
+		public void SetRed(byte red)
+		{
+			colour = colour & 0x00FFFFFF;
+			colour = colour | (uint)(red << 24);
+		}
+		public byte GetRed()
+		{
+			return (byte)((colour & 0xFF000000) >> 24);
+		}
+
+		public void SetGreen(byte green)
+		{
+			colour = colour & 0xFF00FFFF;
+			colour = colour | (uint)(green << 16);
+		}
+		public byte GetGreen()
+		{
+			return (byte)((colour & 0x00FF0000) >> 16);
+		}
+		public void SetBlue(byte blue)
+		{
+			colour = colour & 0xFFFF00FF;
+			colour = colour | (uint)(blue << 8);
+		}
+		public byte GetBlue()
+		{
+			return (byte)((colour & 0x0000FF00) >> 8);
+		}
+		public void SetAlpha(byte alpha)
+		{
+			colour = colour & 0xFFFFFF00;
+			colour = colour | (uint)(alpha << 0);
+		}
+		public byte GetAlpha()
+		{
+
+			return (byte)((colour & 0x000000FF) >> 0);
+		}
+		
+	}
+}
---
+++ MathLibrary/Vector3.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public struct Vector3
+    {
+        public float x, y, z;
+
+        public Vector3(float x, float y, float z)
+        {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+        }
+
+		//Operator overloading
+
+		// V = V + V (point translated by a vector)
+		public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+			return result;
+		}
+
+		// V = V –V (point translated by a vector)
+		public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result = new Vector3();
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+			return result;
+		}
+		// V = V x f(vector scale)
+		public static Vector3 operator *(Vector3 lhs, float rhs)
+		{
+			Vector3 result = new Vector3();
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+			return result;
+		}
+		// V = f x V (vector scale)
+		public static Vector3 operator *(float lhs, Vector3 rhs)
+		{
+			Vector3 result = new Vector3();
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+			return result;
+		}
+		// f = V.Magnitude()
+		public float Magnitude()
+		{
+			return (float)Math.Sqrt((x * x) + (y * y) + (z * z));
+		}
+		// V.Normalise()
+		public float Normalize()
+		{
+			float magnitude = Magnitude();
+			if (magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				z /= magnitude;
+				return magnitude;
+			}
+			return magnitude;
+		}
+		public float Dot(Vector3 rhs)
+		{
+			return ((x * rhs.x) + (y * rhs.y) + (z * rhs.z));
+		}
+		public Vector3 Cross(Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = (y * rhs.z) - (z * rhs.y);
+			result.y = (z * rhs.x) - (x * rhs.z);
+			result.z = (x * rhs.y) - (y * rhs.x);
+			return result;
+		}
+	}
+}
